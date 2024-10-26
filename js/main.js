dfbb349ce1478287553a57d38685542e7ba3a3ed
Dach1980 <145884906+Dach1980@users.noreply.github.com>
2024-10-27 07:42:35 +1000

    Исправляет замечания наставника

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+// Количество сгенерированных объектов
+const SIMILAR_USER_COUNT = 25;
+
+// Основные магические числа
+const PHOTO_MIN = 1;
+const PHOTO_MAX = 25;
+const LIKES_MIN = 15;
+const LIKES_MAX = 200;
+const COMMENTS_MIN = 0;
+const COMMENTS_MAX = 30;
+const AVATAR_MIN = 1;
+const AVATAR_MAX = 6;
+
+// Возможные описания фотографий
+const DESCRIPTIONS = [
+  'Закат над морем.',
+  'Горный пейзаж.',
+  'Семейный пикник.',
+  'Морская прогулка.',
+  'Техно-арт.',
+  'На отдыхе.'
+];
+
+// Возможные сообщения для комментариев
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
 // Возможные имена для комментаторов
 const NAMES = [
   'Артём',
@@ -14,28 +47,8 @@ const NAMES = [
   'Ирина'
 ];
 
-// Возможные сообщения для комментариев
-const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-// Возможные описания фотографий
-const DESCRIPTIONS = [
-  'Закат над морем.',
-  'Горный пейзаж.',
-  'Семейный пикник.',
-  'Морская прогулка.',
-  'Техно-арт.',
-  'На отдыхе.'
-];
-
-// Количество сгенерированных объектов
-const SIMILAR_USER_COUNT = 25;
+// Метод по выбору случайного числа
+let GetRandomElementArray = getRandomInteger(0, DESCRIPTIONS.length - 1);
 
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -44,18 +57,18 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-const userCommentObject = () => ({
-  id: getRandomInteger(1, 25),
-  url: String('photos/' + getRandomInteger(1, 25) + '.jpg'),
-  description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
+const createDataPhoto = () => ({
+  id: getRandomInteger(1, SIMILAR_USER_COUNT),
+  url: String('photos/' + getRandomInteger(1, SIMILAR_USER_COUNT) + '.jpg'),
+  description: DESCRIPTIONS[GetRandomElementArray],
   comments: {
-    commentId: getRandomInteger(1, 25), // ? повторить значение id
+    commentId: getRandomInteger(1, SIMILAR_USER_COUNT), // ? повторить значение id
     avatar: String('img/avatar-' + getRandomInteger(1, 6) + '.svg'),
-    message: MESSAGES[getRandomInteger(0, MESSAGES.length - 1)], // ? случайно выбрать одно-два сообщения
+    message: MESSAGES[GetRandomElementArray], // ? случайно выбрать одно-два сообщения
     name: NAMES[getRandomInteger(0, NAMES.length - 1)],
   }
 });
 
-const similarUserObject = Array.from({ length: SIMILAR_USER_COUNT }, userCommentObject);
+const similarUserObject = Array.from({ length: SIMILAR_USER_COUNT }, createDataPhoto);
 
 similarUserObject();
